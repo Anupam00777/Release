6e603df4a40f2e4ca6da282989df30c41a6603d3
Anupam Pandey <anupam2004pandey@gmail.com>
2024-02-08 15:15:29 +0530

    using /api/*  to direct all api requests

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const { auto_login, user_signup, user_login, test } = require("./app");
 module.exports = async (req, res) => {
   const { pathname } = new URL(req.url, process.env.PUBLIC_URL);
 
-  if (pathname === "/auto_login") {
+  if (pathname === "/api/auto_login") {
     return await auto_login(req, res);
-  } else if (pathname === "/user_signup") {
+  } else if (pathname === "/api/user_signup") {
     return await user_signup(req, res);
-  } else if (pathname === "/user_login") {
+  } else if (pathname === "/api/user_login") {
     return await user_login(req, res);
-  } else if (pathname === "/test") {
+  } else if (pathname === "/api/test") {
     return await test(req, res);
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
