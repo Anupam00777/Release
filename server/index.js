b1afa4bbb4d7f168c2ff822a00a9c6cd28108ec3
Anupam Pandey <anupam2004pandey@gmail.com>
2024-02-08 14:13:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const { auto_login, user_signup, user_login, test } = require("./app");
 
-module.exports = {
-  auto_login,
-  user_signup,
-  user_login,
-  test,
+module.exports = async (req, res) => {
+  const { pathname } = new URL(req.url, "http://localhost");
+
+  if (pathname === "/auto_login") {
+    return await auto_login(req, res);
+  } else if (pathname === "/user_signup") {
+    return await user_signup(req, res);
+  } else if (pathname === "/user_login") {
+    return await user_login(req, res);
+  } else {
+    // Handle other routes or return a default response
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not found");
+  }
 };
